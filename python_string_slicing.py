2dfb801ed431eb7dd035fc15c5f34dbcbf976b34
NaveenSurada <102537036+NaveenSurada@users.noreply.github.com>
2022-04-22 11:48:29 +0530

    Add files via upload

---
+++ python_string_slicing.py
@@ -0,0 +1,29 @@
+##var = "dhoni"
+##print(var[0])
+##print(var[-5])
+##d - 0,-5
+##h - 1,-4
+##o - 2,-3
+##n - 3,-2
+##i - 4,-1
+
+var = "virat is rcb captain" #string slicing can't happen on reverse direction
+##print(var[0])
+##print(var[0:])
+##print(var[0:5])
+##print(var[6:2]) this one is in reverse order
+##print(var[0:4])
+##print (var[2:3])
+##print(var[5:6])
+##print(var[0:6])
+##print(var[0:])
+##print(var[:])
+##print(var[1:4])
+##print(var[0:1])
+##print (var[1:])
+##print(var[2:-1])
+##print(var[:-3])
+##print(var[0:-5])
+##print(var[::-1])
+##print(var[::-3])
+
