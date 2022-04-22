fcee6434139bbe5c520a2a3022369b01c8728a92
NaveenSurada <102537036+NaveenSurada@users.noreply.github.com>
2022-04-22 11:55:47 +0530

    git

---
+++ python_som functions_apr1.py
@@ -0,0 +1,49 @@
+##var= {"name":"dhoni","age":33,"book":"python", "countrty":"india"}
+##output= dict(sorted(var.items()))
+##print(output)
+
+##var= {"name":"dhoni","age":33,"book":"python", "countrty":"india"}
+##var.pop("book")
+##print(var)
+
+##var= {"name":"dhoni","age":33,"book":"python", "countrty":"india"}
+##var.popitem()
+##print(var)
+
+##var= {"name":"dhoni","age":33,"book":"python", "countrty":"india"}
+##var.clear()
+##print(var)
+
+##var= {}
+##for x in range (4):
+##    var[x]  =x*x
+##print (var)
+
+##var= {}
+##for x in range (5):
+##    var[x]= x*x*x
+##print(var)
+
+##var= {}
+##for x in range (5):
+##    var[x]= x*x*x
+##    print(var)
+
+##var=[4,6,7,3,2]
+##output= var.sort()
+##print(output)
+
+##var=[4,6,7,3,2]
+##var.sort()
+##print(var)
+
+##var="india his rich history"
+##output= var.rfind ("t")
+##print(output)
+##print("welcome to python")
+
+##var = "india his rich history"
+##output= var.rindex("i")
+##print(output)
+##print("welcome to python")
+
