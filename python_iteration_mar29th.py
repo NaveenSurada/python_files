316f037b4267debdd1fa04a2dc6838d398ee12f4
NaveenSurada <102537036+NaveenSurada@users.noreply.github.com>
2022-04-22 11:54:24 +0530

    git

---
+++ python_iteration_mar29th.py
@@ -0,0 +1,76 @@
+##data = "india is mysterious"
+##for x in data:
+##    print(x)
+
+##data = "india is mysterious"
+##for x in data:
+##    print(x,end="")
+
+##data = "india is mysterious"
+##for x in data:
+##    print(x,end=" ")
+
+##data = "india is mysterious"
+##for x in data:
+##    print(x,end="*")
+
+##data = "india is mysterious"
+##for x in enumerate(data):
+##    print(x)
+
+##data = "india is mysterious"
+##for x, y in enumerate(data):
+##    print(x)
+    
+
+##data = "india is mysterious"
+##for x, y in enumerate(data):
+##    print(y)
+    
+##data = "india is mysterious"
+##for x, y in enumerate(data):
+##    print(x)
+##    print(y)
+
+##data = "india is mysterious"
+##my_counter = 0
+##for x in(data):
+##    print(x)
+##    my_counter = my_counter + 1
+##print(my_counter)
+
+##data = "india is mysterious"
+##for b in (data) :
+##    if b == "i":
+##        print(b,"success")
+##    elif b == "s":
+##        print(b,"failure")
+##    else:
+##        print(b,"failure")
+
+##data = "india is mysterious"
+##for b in (data) :
+##    if b == "i" or b== "s":
+##        print(b,"success")
+##    elif b == "s":
+##        print(b,"failure")
+
+##data = "india is mysterious"
+##for b in (data) :
+##    if b == "i" or b== "s":
+##        print(b,"success")
+##        break
+##    else:
+##        print(b,"failure")
+
+##data = "india is mysterious"
+##for b in (data) :
+##    if b == "i" or b== "s":
+##        break
+##    print(b,"success")
+        
+##data = "india is mysterious"
+##for b in (data) :
+##    if b == "i" or b== "s":
+##        continue
+##    print(b,"success")
